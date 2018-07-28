173c88a08e49ad74b2cb1dcac423364c259f2d5c
etopiei <lj343@icloud.com>
2018-07-28 18:07:24 +1000

    More work on inventory view

--- main/fantasy-tube/src/components/Inventory.vue
+++ main/fantasy-tube/src/components/Inventory.vue
@@ -1,8 +1,23 @@
 <template>
+  <div>
+    <div v-for="player in players" :key="player.id">
+      <div> {{ player.name }}
+      <img :src="player.channel_logo_url" class="logo">
+      Price: {{ player.price }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'Inventory',
+  data() {
+    return {
+      players: []
+    }
+  }
+}
 </script>
 
 <style scoped>
