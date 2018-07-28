7d8682520cf4ea7d7913ca93454240c0435b5a21
Dylan Kay <dylan.kay@me.com>
2018-07-28 23:10:12 +1000

    market page groundwork

--- main/fantasy-tube/src/router/index.js
+++ main/fantasy-tube/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import TeamBuild from '@/components/TeamBuild'
 import Inventory from '@/components/Inventory'
-
+import Market from '@/components/Market'
 Vue.use(Router)
 
 export default new Router({
@@ -22,6 +22,11 @@ export default new Router({
       path: '/inventory',
       name: 'Inventory',
       component: Inventory
+    },
+    {
+      path: '/market',
+      name: 'Market',
+      component: Market
     }
   ]
 })
