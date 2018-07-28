9fc5cd0fcda99113b4c989184fb96331da6b6673
Dylan Kay <dylan.kay@me.com>
2018-07-28 20:44:50 +1000

    Fix merged error

--- main/fantasy-tube/src/components/Inventory.vue
+++ main/fantasy-tube/src/components/Inventory.vue
@@ -11,12 +11,6 @@
       </div>
     </div>
   </div>
-  <div>
-    <ChannelThumbnail v-bind:channel="test" active="true"></ChannelThumbnail>
-    <ChannelThumbnail v-bind:channel="test"></ChannelThumbnail>
-    <ChannelThumbnail v-bind:channel="test"></ChannelThumbnail>
-    <ChannelThumbnail v-bind:channel="test"></ChannelThumbnail>
-  </div>
 
 </template>
 
